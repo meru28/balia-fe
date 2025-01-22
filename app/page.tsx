2637a5a0926282465e8ed5637646cb3aba7f89af
merurw <meru_r_wirawan@ekahospital.com>
2025-01-22 17:16:00 +0700

    tes fix dockerfile

--- app/page.tsx
+++ app/page.tsx
@@ -613,7 +613,7 @@ export default function Home() {
                     Simple productivity
                   </h2>
                   <p className="mt-3 text-xl text-white">
-                    Endless tasks, limited hours, a single piece of paper. Not really a haiku, but we're doing our best
+                    Endless tasks, limited hours, a single piece of paper. Not really a haiku, but were doing our best
                     here. No kanban boards, burndown charts, or tangled flowcharts with our Focus system. Just the
                     undeniable urge to fill empty circles.
                   </p>
