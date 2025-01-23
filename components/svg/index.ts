a4092b2408a428cd041ae52f2c747b8f5a578060
merurw <meru_r_wirawan@ekahospital.com>
2025-01-23 23:03:01 +0700

    Update 23 Jan 25: - Layout Loader - Site Navigation - Hero Component - Shop Collection

--- components/svg/index.ts
+++ components/svg/index.ts
@@ -1,2 +1,2 @@
-export { default as Logo1 } from '@/components/svg/logo/Balia-1.svg';
-export { default as Logo2 } from '@/components/svg/logo/Balia-3.svg';
+export { default as Logo1 } from './logo/Balia-1.svg';
+export { default as Logo2 } from './logo/Balia-3.svg';
