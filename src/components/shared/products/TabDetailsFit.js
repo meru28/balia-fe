eb7924759bfa17f24e5b1a91136dfdc81e328a88
meru_r_wirawan <meru_r_wirawan@ekahospital.com>
2025-03-04 16:56:52 +0700

    Update HTML entities for model height in TabDetailsFit
    Replaced single and double quotes with their respective HTML entities to ensure proper rendering and compliance with HTML standards. This change improves consistency and prevents potential rendering issues in different environments.

--- src/components/shared/products/TabDetailsFit.js
+++ src/components/shared/products/TabDetailsFit.js
@@ -7,7 +7,7 @@ const TabDetailsFit = () => {
         <li>Regular fit</li>
         <li>Length: 27.5 inches (size M)</li>
         <li>Model is wearing size M</li>
-        <li>Model height: 5'9" (175cm)</li>
+        <li>Model height: 5&apos;9&quot; (175cm)</li>
         <li>Comfortable stretch material</li>
         <li>Premium stitching details</li>
       </ul>
